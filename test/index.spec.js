9e8c61abad0d2edf9efa2e228146b8a8ffdac9a5
Andrew Hansen <arahan@uw.edu>
2018-05-09 21:29:03 -0700

    wip

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,6 +1,40 @@
 /* eslint-disable no-undef, no-param-reassign, func-names, prefer-arrow-callback */
 const { storyFolders } = require('../shared/storyFolders')
 
+const targetUrlIndex = process.argv.indexOf('--url')
+
+const BASE_URL = process.argv[targetUrlIndex + 1]
+
+function getStories(content) {
+  if (content.namepath) {
+    return content
+  }
+  return Object.values(content).map(getStories)
+}
+
+function isPartialMatch(reference, partial) {
+  return reference.toLowerCase().includes(partial.toLowerCase())
+}
+
+function partialSkipMatch(props) {
+  const { name, folderpath } = props
+
+  return !skip.some(match => isPartialMatch(name, match) || isPartialMatch(folderpath, match))
+}
+
+function formatProperties(props) {
+  const { folderpath, name, image, mobile } = props
+
+  return {
+    label: `${folderpath}__${name}`,
+    image,
+    mobile,
+    url: `${BASE_URL}?selectedKind=${encodeURIComponent(
+      folderpath
+    )}&selectedStory=${encodeURIComponent(name)}`,
+  }
+}
+
 const scenarios = Object.keys(storyFolders)
   .map(kind => ({
     kind,
